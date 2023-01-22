46ddffac374b9e5c223c8597cb0906994552f94b
matias <pagosptcmatias@gmail.com>
2023-01-22 15:03:49 -0300

    agregue el database y las rutas

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import express, { Router } from "express";
 import morgan from "morgan";
 import cors from 'cors'
 import path from "path";
-// import './database'
-// import router from "./routes/producto.routes";
-// import routerUsuario from "./routes/usuarios.routes";
-// import pedidosRouter from './routes/pedidos.routes'
+import './database'
+import router from "./routes/productos.routes";
+import routerUsuario from "./routes/usuarios.routes";
+// import routerPedidos from './routes/pedidos.routes'
 
 const app = express()
 app.set('port', process.env.PORT || 4000)
@@ -19,8 +19,8 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname,'../public')))
-// app.use('/tecnoMas', router)
-// app.use('/tecnoMas/user', routerUsuario)
+app.use('/tecnoMas', router)
+app.use('/tecnoMas/user', routerUsuario)
 // app.use('/tecnoMas', routerPedidos)
 
 
---
+++ src/routes/productos.routes.js
@@ -0,0 +1,77 @@
+import { Router } from "express";
+import { check } from "express-validator";
+import { crearProducto, editarProducto, listarProductos, borrarProducto, obtenerProducto } from "../controllers/productos.controllers";
+
+const router = Router();
+
+
+router.route("/productos")
+  .get(listarProductos)
+  .post(
+//     [
+//     check("nombreProducto")
+//     .notEmpty()
+//     .withMessage("Campo obligatorio")
+//     .isLength({min:2, max:50})
+//     .withMessage("El nobmre del producto debe tener entre 2 y 50 caracteres"),
+//     check("precio")
+//     .notEmpty()
+//     .withMessage("Campo obligatorio")
+//     .isNumeric()
+//     .withMessage("El precio debe ser un número")
+//     .custom((valor)=>{
+//       if(valor >=1 && valor <= 10000){
+//         return true
+//       }else{
+//         throw new Error("El precio debe estar entre 1 y 10000")
+//       }
+//     }),
+//      check("categoria")
+//      .notEmpty()
+//      .withMessage("Seleccione una categoria")
+//     .isIn(["picante epiko", "medio picante", "comida caliente", "comida fria"])
+//     .withMessage("Seleccione una categoria"),
+//     check("imagen")
+//     .notEmpty()
+//     .withMessage("Campo obligatorio")
+//     .matches(/^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/)
+//     .withMessage("Ingrese una URL valida")
+//   ],
+    crearProducto);
+
+router.route("/productos/:id")
+  .get(obtenerProducto)
+  .put(
+//     [
+//     check("nombreProducto")
+//     .notEmpty()
+//     .withMessage("Campo obligatorio")
+//     .isLength({min:2, max:50})
+//     .withMessage("El nobmre del producto debe tener entre 2 y 50 caracteres"),
+//     check("precio")
+//     .notEmpty()
+//     .withMessage("Campo obligatorio")
+//     .isNumeric()
+//     .withMessage("El precio debe ser un número")
+//     .custom((valor)=>{
+//       if(valor >=1 && valor <= 10000){
+//         return true
+//       }else{
+//         throw new Error("El precio debe estar entre 1 y 10000")
+//       }
+//     }),
+//      check("categoria")
+//      .notEmpty()
+//      .withMessage("Seleccione una categoria")
+//     .isIn(["picante epiko", "medio picante", "comida caliente", "comida fria"])
+//     .withMessage("Seleccione una categoria"),
+//     check("imagen")
+//     .notEmpty()
+//     .withMessage("Campo obligatorio")
+//     .matches(/^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/)
+//     .withMessage("Ingrese una URL valida")
+//   ],
+  editarProducto)
+  .delete(borrarProducto)
+
+export default router;
