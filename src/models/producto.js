57b2b07036ed89782976ff8632bbceb9b540a07e
matias <pagosptcmatias@gmail.com>
2023-01-27 23:46:33 -0300

    realice todo el modelado de la nueva parte de productos

--- src/models/producto.js
+++ src/models/producto.js
@@ -7,7 +7,10 @@ const productoSchema = new Schema({
     minLength: 2,
     maxLength: 50,
   },
- 
+  marca: {
+    type: String,
+    required: true,
+  },
 
   precio: {
     type: Number,
@@ -15,21 +18,68 @@ const productoSchema = new Schema({
     minLength: 1,
     maxLength: 1000000,
   },
-  detalle: {
-    type: String,
-    required: true,
-    minLength: 1,
-    maxLength: 5000,
-  },
-  categoria: {
-    type: String,
-    required: true,
-  },
+  
+  
   imagen: {
     type: String,
     required: true,
     value: /^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/,
   },
+  pantalla: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 5000,
+  },
+  procesador: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 5000,
+  },
+  android: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 5000,
+  },
+  interna: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 5000,
+  },
+  expandible: {
+   
+      type: String,
+      required: true,
+    
+  },
+  ram: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 5000,
+  },
+  camara: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 5000,
+  },
+  
+  
+  liberado: {
+    type: String,
+    required: true,
+  },
+  resena: {
+    type: String,
+    required: true,
+    minLength: 100,
+    maxLength: 10000,
+  },
+
 });
 
 const Producto = mongoose.model("producto", productoSchema);
